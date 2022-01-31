3832b30644a39dd1111ce513298dc951ff128cc0
Miguel Román Párraga <mromparb@g.educaand.es>
2022-01-31 14:11:00 +0100

    Solucioon

--- src/main/java/com/example/Calculadora.java
+++ src/main/java/com/example/Calculadora.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Calculadora {
     public int multiplicar(int a, int b) {
-        return a * b + 1;
+        return a * b;
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,11 +1,7 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 /**
